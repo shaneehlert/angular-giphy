5ddea506d62a119d2994bc50b90de1a3c8ceb0f8
shaneehlert <shaneehlert1@gmail.com>
2016-10-26 11:19:27 -0700

    test

--- js/app.js
+++ js/app.js
@@ -2,4 +2,20 @@ angular.module('GiphyApp', [])
 
 .run(function() {
   console.log('App has loaded!');
-});
+})
+.controller('GiphyCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.giphy = [];
+
+  $scope.getGiphy = function() {
+    var req = {
+      url: 'http://api.giphy.com/v1/gifs/search?q=seahawks&api_key=dc6zaTOxFJmzC',
+      method: 'GET'
+    }
+
+    $http(req).then(function success(res) {
+      $scope.giphy = res.data.value;
+    }, function error(res) {
+      console.log(res);
+    });
+  }
+}]);
